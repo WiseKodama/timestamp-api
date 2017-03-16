6986b58b14675735379612892271eb6424312e8d
Nejc Trstenjak <nejc.trstenjak@gmail.com>
2017-03-16 12:40:46 +0000

    added index.html

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var path = require('path');
+
+app.get('/',function(req,res){
+  var fileName = path.join(__dirname, 'index.html');
+  res.sendFile(fileName, function(err){
+    if(err) 
+    res.status(err.status).end();
+  });
+});
 
 app.use('/', function (req, res) {
   var lookUp = decodeURI(req.originalUrl.slice(-(req.originalUrl.length-1)));
   if(Number.isInteger(Date.parse(lookUp))){
     var myDate = new Date(Date.parse(lookUp));
-    res.send({
-      "unix" : (myDate/1000), 
-      "natural" : myDate.toDateString()
+    res.json({
+      unix : (myDate/1000), 
+      natural : myDate.toDateString()
     });
   } 
   else if(Number.isInteger(new Date(lookUp*1000).valueOf())){
     var myDate = new Date(lookUp*1000);
-    res.send({
-      "unix" : (myDate/1000), 
-      "natural" : myDate.toDateString()
+    res.json({
+      unix : (myDate/1000), 
+      natural : myDate.toDateString()
     });
   }
   else{
-   res.send({
-      "unix" : null, 
-      "natural" : null
+   res.json({
+      unix : null, 
+      natural : null
     });
   }
 });
