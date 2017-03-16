3e3e55971ef1c9eeab24257bfb73cc757629066f
Nejc Trstenjak <nejc.trstenjak@gmail.com>
2017-03-16 11:47:29 +0000

    finished timestamp app

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send("farts");
+app.use('/', function (req, res) {
+  var lookUp = decodeURI(req.originalUrl.slice(-(req.originalUrl.length-1)));
+  if(Number.isInteger(Date.parse(lookUp))){
+    var myDate = new Date(Date.parse(lookUp));
+    res.send({
+      "unix" : (myDate/1000), 
+      "natural" : myDate.toDateString()
+    });
+  } 
+  else if(Number.isInteger(new Date(lookUp*1000).valueOf())){
+    var myDate = new Date(lookUp*1000);
+    res.send({
+      "unix" : (myDate/1000), 
+      "natural" : myDate.toDateString()
+    });
+  }
+  else{
+   res.send({
+      "unix" : null, 
+      "natural" : null
+    });
+  }
 });
 
 app.listen(8080);
